6838bb9a6d4a240ddc1f777b2e5f46ea84e90237
Tyowk <tyowk.personal@gmail.com>
2024-12-12 11:11:39 +0700

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -18,33 +18,78 @@ interface Vote {
     bot?: string;
     guild?: string;
     isWeekend?: boolean;
+    query?: string[];
     type: string;
 }
 
+interface Votes {
+    username: string;
+    id: string;
+    avatar: string;
+}
+
 interface Bot {
+      defAvatar: string;
       id: string;
+      clientId: string;
       username: string;
       discriminator: string;
       avatar: string;
       prefix: string;
-      library: string;
-      description: string;
+      longdesc: string;
+      shortdesc: string;
+      date: string;
+      server_count: number;
+      shard_count: number;
       tags: string[];
       website: string;
       github: string;
       support: string;
       invite: string;
+      guilds: string[];
       owners: string[];
-      votes: number;
-      servers: number;
-      shards: number;
-      shardCount: number;
+      monthlyPoints: number;
+      points: number;
+      certifiedBot: boolean;
+      donatebotguildid: string;
+}
+interface Socials {
+    youtube: string;
+    reddit: string;
+    instagram: string;
+    github: string;
+    twitter: string;
+}
+
+interface User {
+    discriminator: string;
+    avatar: string;
+    id: string;
+    username: string;
+    defAvatar: string;
+    admin: boolean;
+    webMod: boolean;
+    mod: boolean;
+    certifiedDev: boolean;
+    supporter: boolean;
+    social: Socials;
+    bio?: string;
+    banner?: string;
+    color?: string;
+}
+
+interface GetStats {
+    results: Bot[];
+    limit: number;
+    offset: number;
+    count: number,
+    total: number
 }
 
 interface Stats {
-    serverCount: number;
-    shards: number;
-    shardCount: number;
+    server_count: number;
+    shards: number[];
+    shard_count: number;
 }
 
 interface RESTOptions {
@@ -57,17 +102,22 @@ interface ManagerOptions {
     webhook?: WebhookOptions;
 }
 
+interface Voted {
+    voted: number;
+}
+
 interface REST extends EventEmitter {
     token: string;
     authorization: string;
     port: number;
     endpoint: string;
     constructor(options: RESTOptions);
-    getBots(limit?: number, offset?: number, sort?: string, fields?: string): Promise<Bot[] | null>;
+    getBots(limit?: number, offset?: number, sort?: string, fields?: string): Promise<GetStats | null>;
     getBot(id: string): Promise<Bot | null>;
-    getBotVotes(id: string): Promise<Vote[] | null>;
+    getUser(id: string): Promise<User | null>;
+    getBotVotes(id: string): Promise<Votes[] | null>;
     getBotStats(id: string): Promise<Stats | null>;
-    checkUser (id: string, userId: string): Promise<Vote | null>;
+    checkUser (id: string, userId: string): Promise<Voted | null>;
     postStats(id: string, stats: Stats): Promise<boolean>;
 }
 
